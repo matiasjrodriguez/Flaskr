868a374aa12b0e076fe968b335bdd5e62de44d85
Matías Javier Rodriguez <mellarodriguez29@gmail.com>
2022-05-21 03:55:58 -0300

    Función para autentificar en otras vistas

--- app/auth.py
+++ app/auth.py
@@ -79,3 +79,12 @@ def load_logged_in_user():
         g.user = get_db().execute(
             'SELECT * FROM user WHERE id = ?', (user_id,)
         ).fetchone()
+    
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+
+    return wrapped_view
